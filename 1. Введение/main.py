039286953ecaa176a8e92038026701a0c0978652
Андрей Боев <a.boev.it@gmail.com>
2025-11-20 16:07:33 +0300

    2. Операции деления

---
+++ 1. Введение/main.py
@@ -0,0 +1,9 @@
+"""
+print("\"Hello World!\"")
+"""
+
+
+name = input("Введите ваше имя: ")
+age = int(input("Введите ваш возраст: "))
+print('Меня зовут ', name, ' и мне ', age, ' лет.', sep='')
+print(f'Меня зовут {name} и мне {age} лет.')
