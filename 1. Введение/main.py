eabdc23499e5d6c1b3a576c97cd4e409b9e792c7
Катя <meoweee89@yandex.ru>
2025-11-20 16:10:23 +0300

    Решение задач

---
+++ 1. Введение/main.py
@@ -0,0 +1,6 @@
+for a in 0,1:
+    for b in 0,1:
+        for c in 0,1:
+            for d in 0,1:
+                f = ((a and b) == (not c)) and (b <= d)
+                print (a,b,c,d)
---
+++ 1. Введение/Задания/Задание 10.py
@@ -0,0 +1,14 @@
+from math import sqrt
+
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+x3 = int(input())
+y3 = int(input())
+a = sqrt ((x2 - x1)**2 + (y2 - y1)**2)
+b = sqrt ((x3 - x2)**2 + (y3 - y2)**2)
+c = sqrt ((x1 - x3)**2 + (y1 - y3)**2)
+p = (a+b+c)/2
+print(f'Площадь {sqrt(p*(p-a)*(p-b)*(p-c))}')
+print(f'Периметр {p*2}')
