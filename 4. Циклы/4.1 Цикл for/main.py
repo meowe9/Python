d75542fb6adcc8bbb8dd82199986468ab24f5af5
Андрей Боев <a.boev.it@gmail.com>
2025-12-04 16:56:39 +0300

    3. Циклы

---
+++ 4. Циклы/4.1 Цикл for/main.py
@@ -0,0 +1,22 @@
+"""
+for i in 'Text 1':
+    print(i, end=' ')
+"""
+
+for i in range(10):
+    print(i, end=' ')
+
+print()
+
+for i in range(5, 10):
+    print(i, end=' ')
+
+print()
+
+for i in range(2, 15, 3):
+    print(i, end=' ')
+
+print()
+
+for i in range(10, 0, -1):
+    print(i, end=' ')
